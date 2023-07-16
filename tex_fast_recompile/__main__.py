d27705b700abffb818df5014e725bc72ad4256d9
user202729 <25191436+user202729@users.noreply.github.com>
2023-07-16 14:20:19 +0700

    Change check-version method

--- tex_fast_recompile/__main__.py
+++ tex_fast_recompile/__main__.py
@@ -176,7 +176,7 @@ class CompilationDaemonLowLevel:
 			compiling_filename=self.filename
 
 		else:
-			compiling_filename=r"\RequirePackage[check-version=0.4.0]{fastrecompile}"
+			compiling_filename=r"\RequirePackage{fastrecompile}\fastrecompilecheckversion{0.4.0}"
 			if preamble.implicit:
 				if self.pause_at_begindocument_end:
 					compiling_filename+=r"\fastrecompilesetimplicitpreambleii"
